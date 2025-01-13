4d499bd290e8407228366b89eb20b2222a086dd9
Tadasei <chemselhak.mahouche@gmail.com>
2025-01-13 19:41:44 +0100

    added projects information notice to Projects view

--- assets/Contact-B7kKtQ7I.js
+++
@@ -1,1 +0,0 @@
-import{f as t,L as e,u as a,o as c,O as o,S as n}from"./index-T7g4IS2C.js";const u={__name:"Contact",setup(r){return(s,_)=>(c(),t(a(n),{class:"container mx-auto text-center"},{content:e(()=>[o("Contact")]),_:1}))}};export{u as default};
---
+++ assets/Contact-COMdj34g.js
@@ -0,0 +1,1 @@
+import{f as t,L as e,u as a,o as c,O as o,S as n}from"./index-BO576FMd.js";const u={__name:"Contact",setup(r){return(s,_)=>(c(),t(a(n),{class:"container mx-auto text-center"},{content:e(()=>[o("Contact")]),_:1}))}};export{u as default};
